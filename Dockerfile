f2fe6a3ed2d76d02f4990b5a1626bd3c9070402b
mukulmj <mukul.joshi@opstree.com>
2025-02-10 17:02:13 +0530

    initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,14 @@
-FROM alpine
+FROM alpine:latest
 RUN apk add --no-cache --upgrade bash
-RUN apk add jq
+RUN apk add jq yq
+
 COPY build.sh .
+RUN chmod +x build.sh
 
 ADD BP-BASE-SHELL-STEPS /opt/buildpiper/shell-functions/
 
-
 ENV SLEEP_DURATION 5s
-ENV ACTIVITY_SUB_TASK_CODE REPLACE_IT
+ENV ACTIVITY_SUB_TASK_CODE BP-DEPLOYMENT-MANIFEST-EDITOR
 ENV VALIDATION_FAILURE_ACTION WARNING
 
 ENTRYPOINT [ "./build.sh" ]
--- build.sh
+++ build.sh
@@ -1,24 +1,41 @@
 #!/bin/bash
+
 source /opt/buildpiper/shell-functions/functions.sh
 source /opt/buildpiper/shell-functions/log-functions.sh
-source /opt/buildpiper/shell-functions/str-functions.sh
-source /opt/buildpiper/shell-functions/file-functions.sh
-source /opt/buildpiper/shell-functions/aws-functions.sh
 
 TASK_STATUS=0
 
-CODEBASE_LOCATION="${WORKSPACE}"/"${CODEBASE_DIR}"
-logInfoMessage "I'll do processing at [$CODEBASE_LOCATION]"
-sleep  $SLEEP_DURATION
-cd  "${CODEBASE_LOCATION}"
+CODEBASE_LOCATION="/bp/data/k8s_manifest"
+DEPLOYMENT_FILE="$CODEBASE_LOCATION/deployment.yaml"
 
-TASK_STATUS=0
+logInfoMessage "I'll build the code available at [$CODEBASE_LOCATION]"
 
-if [condition]; then
-    logErrorMessage "Done the required operation"
-else
-    TASK_STATUS=1
-    logErrorMessage "Target server not provided please check"
+# # Check if yq is installed
+# yq --version &>/dev/null
+# if [ $? -ne 0 ]; then
+#     logErrorMessage "yq is not installed. Please install it before running this script."
+#     exit 1
+# fi
 
+# Check if deployment.yaml exists
+if [ ! -f "$DEPLOYMENT_FILE" ]; then
+    logErrorMessage "Deployment file not found at $DEPLOYMENT_FILE"
+    exit 1
 fi
+
+TASK_STATUS=$?
+
+# Check if NEW_SERVICE_ACCOUNT is provided
+if [[ -z "$NEW_SERVICE_ACCOUNT" ]]; then
+    logWarningMessage "No new service account name provided. Continuing with the pre-configured name."
+    NEW_SERVICE_ACCOUNT=$(yq e '.spec.template.spec.serviceAccountName' "$DEPLOYMENT_FILE")
+    logInfoMessage "Using pre-configured service account: $NEW_SERVICE_ACCOUNT"
+else
+    logInfoMessage "Updating serviceAccountName to: $NEW_SERVICE_ACCOUNT"
+    yq e -i ".spec.template.spec.serviceAccountName = \"$NEW_SERVICE_ACCOUNT\"" "$DEPLOYMENT_FILE"
+    logSuccessMessage "serviceAccountName updated successfully!"
+fi
+
+TASK_STATUS=$?
+
 saveTaskStatus ${TASK_STATUS} ${ACTIVITY_SUB_TASK_CODE}
